1d8914dc59826242759275626b72066786b5de0c
MaxiSound <maxisound@yandex.ru>
2023-07-06 16:33:56 +0700

    ready

--- Homework_8/task_1.py
+++ Homework_8/task_1.py
@@ -10,13 +10,12 @@ import json
 
 def convert_to_json(file_name):
     dic = {}
-    with (open(file_name, "r", encoding="UTF-8") as f1,
-          open('task_1.json', "w", encoding="UTF-8") as f2):
+    with (open(file_name, "r", encoding="UTF-8") as f1, open('task_1.json', "w", encoding="UTF-8") as f2):
         dic = {}
         for i in f1:
             k, v = (i.rstrip()).split(':')
             dic[k.capitalize()] = v
-        json.dump(dic, f2, ensure_ascii=False)
+            json.dump(dic, f2, ensure_ascii=False)
 
 
 convert_to_json('Homework_7_task_3_result.txt')
